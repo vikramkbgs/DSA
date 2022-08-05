cdd1fae7f55aa00088b7681bd44b44da1411845f
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-08-05 14:53:18 +0530

    quickSort formate

---
+++ searchingAndSorting/quicksort.java
@@ -0,0 +1,33 @@
+public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public static int partition(int[] arr, int low , int high)
+    {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i <= j)
+        {
+            if(pivot >= arr[i])
+                i++;
+            else if(pivot < arr[j])
+                j--;
+            else 
+            swap(arr, i, j);
+        }
+        swap(arr, low, j);
+    return j;
+    }
+    
+    public static void quickSort(int[] input, int startIndex, int endIndex) {
+			// your code goes here
+        	if(startIndex >= endIndex)
+                return;
+        	int pi = partition(input, startIndex, endIndex);
+            quickSort(input, startIndex, pi - 1);
+        	quickSort(input, pi+1, endIndex);
+		}
