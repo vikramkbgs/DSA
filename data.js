464eb00b7cb758942306b1687aa7ca97320099e0
Vikram Kumar <vikramkbgs@gmail.com>
2023-02-19 18:10:11 +0530

    Loop

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2023-03-29T18:10:10+05:30', code: 'if (condition) {
-	// Statement
+module.exports = { date: '2023-02-19T18:10:11+05:30', code: 'for (let i = 0; i < 10; i++) {
+	// Loop body
 }', functionCode: '' };
