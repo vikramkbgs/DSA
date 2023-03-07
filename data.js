b5ca0d44b55202bda4aba914d706939621834488
Vikram Kumar <vikramkbgs@gmail.com>
2023-03-07 18:09:57 +0530

    Variable Declaration

--- data.js
+++ data.js
@@ -1,3 +1,1 @@
-module.exports = { date: '2023-05-28T18:09:56+05:30', code: '[object Object]', functionCode: 'function function43() {
-	// Function body
-}' };
+module.exports = { date: '2023-03-07T18:09:57+05:30', code: 'var variable9 = 6;', functionCode: '' };
