4b617b606d9296e5c21243ceb01eedbf24e00a14
Vikram Kumar <vikramkbgs@gmail.com>
2023-05-06 18:10:57 +0530

    Variable Declaration

--- data.js
+++ data.js
@@ -1,3 +1,1 @@
-module.exports = { date: '2023-06-06T18:10:57+05:30', code: 'do-while (let i = 0; i < 10; i++) {
-	// Loop body
-}', functionCode: '' };
+module.exports = { date: '2023-05-06T18:10:57+05:30', code: 'var variable25 = 96;', functionCode: '' };
