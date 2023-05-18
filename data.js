97fd3abfdee93d77389f0851b6048852dad3450d
Vikram Kumar <vikramkbgs@gmail.com>
2023-05-18 18:10:01 +0530

    Loop

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2022-10-20T18:10:01+05:30', code: 'else if (condition) {
-	// Statement
+module.exports = { date: '2023-05-18T18:10:01+05:30', code: 'for (let i = 0; i < 10; i++) {
+	// Loop body
 }', functionCode: '' };
