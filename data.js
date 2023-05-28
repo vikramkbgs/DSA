3ae89840c4bcb6ec751f8ff983e11dfc113decf9
Vikram Kumar <vikramkbgs@gmail.com>
2023-05-28 18:11:15 +0530

    Loop

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2022-09-09T18:11:14+05:30', code: 'class Class81 {
-	// Class body
+module.exports = { date: '2023-05-28T18:11:15+05:30', code: 'for (let i = 0; i < 10; i++) {
+	// Loop body
 }', functionCode: '' };
