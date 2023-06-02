d136173dc4b92a2bf43d1ef60634a934d774317b
Vikram Kumar <vikramkbgs@gmail.com>
2023-06-02 18:01:37 +0530

    Variable Declaration

--- data.js
+++ data.js
@@ -1,3 +1,1 @@
-module.exports = { date: '2023-08-28T17:57:30+05:30', code: 'class Class40 {
-	// Class body
-}', functionCode: '' };
+module.exports = { date: '2023-06-02T18:01:37+05:30', code: 'var variable13 = 37;', functionCode: '' };
