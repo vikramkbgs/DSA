5a9a2405fbe4c796990862c2ff7c4f6af6434dcf
Vikram Kumar <vikramkbgs@gmail.com>
2023-07-27 17:33:19 +0530

    Conditional Statement - 2023-07-27T17:33:19+05:30

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2023-10-01T17:33:18+05:30', code: 'class Class50 {
-	// Class body
+module.exports = { date: '2023-07-27T17:33:19+05:30', code: 'if (condition) {
+	// Statement
 }' };
