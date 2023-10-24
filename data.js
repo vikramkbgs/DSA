85fcc1cb662e76a073e5129ea9fd39bedadce83b
Vikram Kumar <vikramkbgs@gmail.com>
2023-10-24 18:04:25 +0530

    Class

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2023-08-25T18:04:25+05:30', code: '[object Object]', functionCode: 'function function50() {
-	// Function body
-}' };
+module.exports = { date: '2023-10-24T18:04:25+05:30', code: 'class Class96 {
+	// Class body
+}', functionCode: '' };
