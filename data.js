dbb8252933070c77dc159e8865873e33e51b3f51
Vikram Kumar <vikramkbgs@gmail.com>
2023-12-29 18:04:32 +0530

    Loop

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2024-04-10T18:04:31+05:30', code: 'do-while (let i = 0; i < 10; i++) {
+module.exports = { date: '2023-12-29T18:04:32+05:30', code: 'while (let i = 0; i < 10; i++) {
 	// Loop body
 }', functionCode: '' };
