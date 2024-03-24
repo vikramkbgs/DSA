b80c262de9713f519b13ef866f8beec63de43dfe
vikramkbgs <vikramkbgs@gmail.com>
2024-03-24 15:57:41 +0530

    20240324 romanToInt

---
+++ Array/romanToNumber.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class romanToNumber{
+
+ public int romanToNumber(String s){
+	HashMap<Character, Interger> alpha = new HashMap<>();
+	alpha.put('I', 1);
+	alpha.put('V', 5);
+	alpha.put('X',10);
+	alpha.put('L',50);
+	alpha.put('C', 100);
+	alpha.put('D',500);
+	alpha.put('M',1000);
+	
+	return 0;
+	
+	}
+	
+	public static void main(String[] args){
+		Solution solution = new Solutin();
+	}
+	
+		
+}
