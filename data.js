1a5199bfde419e81a4ac116656d161b8966604a1
Vikram Kumar <vikramkbgs@gmail.com>
2024-05-27 17:48:04 +0530

    Conditional Statement - 2023-08-15T17:48:04+05:30

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2023-06-22T17:48:03+05:30', code: 'do-while (let i = 0; i < 10; i++) {
-	// Loop body
+module.exports = { date: '2023-08-15T17:48:04+05:30', code: 'else (condition) {
+	// Statement
 }', functionCode: '' };
