29a174eff18fa60c9964e058c2de9b94bf7c7ed7
Vikram Kumar <vikramkbgs@gmail.com>
2024-05-30 11:45:38 +0530

    adding bubbleSort

---
+++ searchingAndSorting/sorting/bubbleSort.java
@@ -0,0 +1,21 @@
+
+public class Solution {
+    
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return;
+    }
+    public static void bubbleSort(int[] arr, int n) {
+        //Your code goes here
+        for (int i = 0; i < n - 1; i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+
+}
