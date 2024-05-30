330f7f4a90f63f26e8e6082d327ebb9d1e7e25f7
Vikram Kumar <vikramkbgs@gmail.com>
2024-05-30 11:37:21 +0530

    adding binarySearch

---
+++ searchingAndSorting/searching/binarySearch.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static int search(int []arr, int x) {
+        // Write your code here.
+        int left, right;
+        left = 0;
+        right = arr.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if (arr[mid] == x){
+                return mid;
+            }
+            if (arr[mid] < x)
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+        return -1;
+    }
+}
