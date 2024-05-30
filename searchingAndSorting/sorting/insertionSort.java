55f704169f61d49b8823f157edf97a9dcfaaf679
Vikram Kumar <vikramkbgs@gmail.com>
2024-05-30 11:47:04 +0530

    adding insertionSort

---
+++ searchingAndSorting/sorting/insertionSort.java
@@ -0,0 +1,19 @@
+
+public class Solution {  
+
+    public static void insertionSort(int[] arr) {
+    	//Your code goes here
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+}
