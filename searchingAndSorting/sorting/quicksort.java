0a0c23b1f84a663ea1698df8c6d9519bf5428ec5
Vikram Kumar <vikramkbgs@gmail.com>
2024-06-07 19:27:16 +0530

    practice number 01

--- searchingAndSorting/sorting/quicksort.java
+++ searchingAndSorting/sorting/quicksort.java
@@ -31,3 +31,38 @@ public static void swap(int[] arr, int i, int j)
             quickSort(input, startIndex, pi - 1);
         	quickSort(input, pi+1, endIndex);
 		}
+
+
+
+public static int partition(int[] arr, int low , int high){
+
+	int pivot = arr[low];
+
+	int i = low;
+
+	int j = high;
+
+	while(i <= j){
+
+		if(pivot >= arr[i])
+			i++;
+		else if( pivot < arr[j])
+			j--;
+		else 
+			swap(arr, i, j);
+	}
+	swap(arr, low, j);
+	return j;
+}
+
+public static void quickSort(int[], int startIndex, int endIndex){
+	if(startIndex >= endIndex)
+		return;
+
+	int pi = partition(input, startIndex, endIndex);
+	quickSort(input, startIndex, pi -1);
+	quicksort(input, pi+1, endIndex);
+}
+
+
+
