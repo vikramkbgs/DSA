1af35feda5627ac923bc45f7998bcba3924f79ec
Vikram Kumar <vikramkbgs@gmail.com>
2024-06-07 19:18:22 +0530

    practice number 01

--- searchingAndSorting/sorting/bubbleSort.java
+++ searchingAndSorting/sorting/bubbleSort.java
@@ -19,3 +19,19 @@ public class Solution {
     }
 
 }
+
+public static void bubbleSort(int[] arr, int n){
+
+	for(int i = 0; i< n -1; i++){
+
+		for(int j = 0; j < n - i- 1; j++){
+
+			if(arr[j] > arr [j + 1]){
+
+				swap(arr, j, j+1);
+			}
+		}
+	}
+}
+
+
