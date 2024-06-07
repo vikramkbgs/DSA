b90673e085b9786550b3b67aa96f6b0602e5535f
Vikram Kumar <vikramkbgs@gmail.com>
2024-06-07 19:09:56 +0530

    binarySearch practice 01

--- searchingAndSorting/searching/binarySearch.java
+++ searchingAndSorting/searching/binarySearch.java
@@ -18,3 +18,23 @@ public class Solution {
         return -1;
     }
 }
+
+publid class Solution{
+	public static int search(int[] arr, int x){
+		int left, right;
+		left = 0;
+		right = arr.length - 1;
+
+		while(left <= right){
+			int mid = left + (right - left)/2;
+			if (arr[mid] == x)
+				return mid;
+			if(arr[mid] < x)
+				left = mid+1;
+			else
+				right = mid - 1;
+		}
+		return -1;
+	}
+}
+
