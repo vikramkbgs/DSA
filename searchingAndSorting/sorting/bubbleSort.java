90230647ff979081042dfd94678a33979a7e3f3c
vikramkbgs <vikramkbgs@gmail.com>
2024-08-14 13:19:59 +0530

    bubbleSort updated

--- searchingAndSorting/sorting/bubbleSort.java
+++ searchingAndSorting/sorting/bubbleSort.java
@@ -1,4 +1,3 @@
-
 public class Solution {
     
     public static void swap(int[] arr, int i, int j){
@@ -18,20 +17,6 @@ public class Solution {
         }
     }
 
-}
-
-public static void bubbleSort(int[] arr, int n){
-
-	for(int i = 0; i< n -1; i++){
-
-		for(int j = 0; j < n - i- 1; j++){
-
-			if(arr[j] > arr [j + 1]){
-
-				swap(arr, j, j+1);
-			}
-		}
-	}
 }
 
 
